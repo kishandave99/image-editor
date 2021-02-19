b4f7e3b6db5825fd919b30b4953aef018d1da292
Kishan Dave <kishandave99@gmail.com>
2021-02-19 11:07:37 +0530

    bug fixes

--- public/config/environment.js
+++ public/config/environment.js
@@ -2,17 +2,13 @@
     window.environment = window.environment || {};
 
     // API url
-    // window.environment.webService = 'https://api.91erp.in/'; //Live server url
-    window.environment.webService = 'https://image-editor99.netlify.app/';
-    // window.environment.webService = 'http://api.91erp.info/'; //test server url
+    window.environment.webService = 'https://image-editor99.netlify.app/api/';
 
     // Base url
     // window.environment.baseUrl = '/image-editor';
     window.environment.baseUrl = '';
 
     // Public Resource url
-    // window.environment.publicResourceUrl = 'https://api.91erp.in/91erp-public-resource/'; //live server url
     window.environment.publicResourceUrl = 'https://image-editor99.netlify.app/';
-    // window.environment.publicResourceUrl = 'http://api.91erp.info/91erp-public-resource/'; //test server url
 
 }(this));
