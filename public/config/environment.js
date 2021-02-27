4c192802f4a1bd74c0c38ceb17e0d64260ae3612
Kishan Dave <kishandave99@gmail.com>
2021-02-27 21:52:57 +0530

    bug fix

--- public/config/environment.js
+++ public/config/environment.js
@@ -2,13 +2,13 @@
     window.environment = window.environment || {};
 
     // API url
-    window.environment.webService = 'https://image-editor99.netlify.app/api/';
+    window.environment.webService = 'https://603a6a57e4f0281a802dcc16--elated-yonath-2c49d7.netlify.app/api/';
 
     // Base url
     // window.environment.baseUrl = '/image-editor';
     window.environment.baseUrl = '';
 
     // Public Resource url
-    window.environment.publicResourceUrl = 'https://image-editor99.netlify.app';
+    window.environment.publicResourceUrl = 'https://603a6a57e4f0281a802dcc16--elated-yonath-2c49d7.netlify.app';
 
 }(this));
